2e7d9180d3bb2b7386cacf6097e3a41fa68603a3
Adam <Adam@AirborneGamer.com>
2017-09-20 16:49:00 -0500

    Initial branch check-in

--- Mastodon/Controllers/HomeController.cs
+++ Mastodon/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Mastodon.Controllers
 {
+
+    //Main controller for landing page
+
     public class HomeController : Controller
     {
         public IActionResult Index()
