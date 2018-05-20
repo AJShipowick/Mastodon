3f5eee0b7eb4d705d2435dd89815afd67bcaec25
Adam Shipowick <AJShipowick@Gmail.com>
2018-05-20 15:49:13 -0500

    test

--- Mastodon/Startup.cs
+++ Mastodon/Startup.cs
@@ -38,7 +38,7 @@ namespace OsOEasy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
