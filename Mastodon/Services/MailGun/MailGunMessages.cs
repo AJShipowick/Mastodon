000106e5f33ee604d79a85a5c6270d2f808c85fd
Adam Shipowick <AJShipowick@Gmail.com>
2018-07-05 08:44:20 -0500

    Changed email msg

--- Mastodon/Services/MailGun/MailGunMessages.cs
+++ Mastodon/Services/MailGun/MailGunMessages.cs
@@ -12,7 +12,7 @@
         #region New Users        
         public const string Subject_NewUser = "{0}, Welcome to OsoEasyPromo";
         public const string Message_NewUser = "Hi {0},\r\n\r\n" +
-                                                    "I'm so glad you decided to try OsOEasyPromo.com for free!\r\n\r\n" +
+                                                    "I'm so glad you decided to try www.OsOEasyPromo.com for free!\r\n\r\n" +
                                                     "Make sure to setup your website script so you can start running unlimited custom promotions on your website right away.\r\n\r\n" +
                                                     "I would love to help you answer any quesitons you may have about our unique service.\r\n" +
                                                     "You can reply directly to this email if you would like.";
@@ -21,7 +21,7 @@
         #region Reset Password        
         public const string Subject_ResetPassword = "Password Reset for OsO Easy Promo";
         public const string Message_ResetPassword = "Hi {0}, we heard you need to reset your password. \r\n\r\n";
-        public const string Action_ResetPassword = "Please reset your OsOEasyPromo.com password by clicking below:\r\n\r\n {0}";
+        public const string Action_ResetPassword = "Please reset your www.OsOEasyPromo.com password by clicking below:\r\n\r\n {0}";
         #endregion
 
         #region Subscribers
