5a5fa62a46c8352b4f151d54b5edb22d5c0593a5
Adam Shipowick <AJShipowick@Gmail.com>
2018-07-22 21:20:24 -0500

    Updated valid contact us form.

--- Mastodon/wwwroot/js/HomePage/homePage.js
+++ Mastodon/wwwroot/js/HomePage/homePage.js
@@ -3,6 +3,8 @@
 /*Begin Oso Easy Promo Script*/(function () { let a = new XMLHttpRequest; a.onreadystatechange = function () { if (4 === a.readyState) if (200 === a.status) { if (document.body.className = 'ok', !a.responseText || a.responseText.includes('ERROR') || a.responseText.includes('WARNING')) return void console.log(a.responseText); let b = document.createElement('script'); b.innerHTML = a.responseText, document.getElementsByTagName('head')[0].appendChild(b) } else console.log('OsO Easy Promo Error' + a.responseText) }, a.open('GET', 'https://api.osoeasypromo.com/api/promo/8899161c-1c6e-4a96-93fd-2870ffad82c4', !0), a.send(null) })();/*End Oso Easy Promo Script*/
 
 function contactUs() {
+    if (!formValid()) { return; }
+
     axios.get('SendContactRequest' + "?userEmail=" + $('#userEmail').val() + "&userComment=" + $('#userComment').val())
         .then(function (response) {
             $('#sendConfirmation').show();
@@ -10,4 +12,15 @@ function contactUs() {
         .catch(function (error) {
             //todo Handle errors
         });
+}
+
+function formValid() {
+    $('#confirmationError').hide();
+
+    if ($('#userEmail').val() && $('#userComment').val()) {
+        return true;
+    } else {
+        $('#confirmationError').show();
+        return false;
+    }
 }
